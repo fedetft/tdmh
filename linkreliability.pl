68676f9e48bf7e8a6a74dbf8f2c7c36c885b09d9
Terraneo Federico <fede.tft@miosix.org>
2018-05-24 16:50:51 +0200

    Added link reliability script

---
+++ linkreliability.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+use strict;
+
+my %links;     # Hash mapping links to number of times they appear
+my $uplinks=0; # Counts the number of uplink rounds
+
+while(<>)
+{
+	if(/(\[\d+ - \d+\])/)
+	{
+		$links{$1}=$links{$1}+1;
+	} elsif(/\[U\] Current topology:/) {
+		$uplinks++;
+	}
+}
+
+foreach(sort keys %links)
+{
+	printf("$_ %7.3f %%\n",$links{$_}/$uplinks*100);
+}
