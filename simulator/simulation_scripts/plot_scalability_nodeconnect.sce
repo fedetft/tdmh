98bbf9e4636c3fd1514d826ceabb47233eadbb06
Federico Terraneo <fede.tft@miosix.org>
2018-09-10 09:29:42 +0200

    Added node connect simulation

---
+++ simulator/simulation_scripts/plot_scalability_nodeconnect.sce
@@ -0,0 +1,55 @@
+clear; clc
+
+x=[2,4,8,16,32,64,128];
+
+function convergence=get_convergence_times(filename,n,mark)
+    a=fscanfMat(filename);
+    ct=a(:,4);
+    convergence=resize_matrix(ct',1,n,%nan);
+    plot2d('ll',x,convergence,-mark);
+endfunction
+
+function print_convergence(convergence)
+    printf("Node connect time [s] as a function of N_max=maximum number of\n");
+    printf("node (TDMH-MAC configuration parameter) and N=actual numer of nodes\n")
+    printf("\t\tN=2\tN=4\tN=8\tN=16\tN=32\tN=64\tN=128\n");
+    [r,c]=size(hex);
+    for i=1:r
+        printf("N_max=%d ",x(length(x)+1-i));
+        for j=1:c
+            if isnan(convergence(i,j))==%F then
+                printf("\t%3.1f",convergence(i,j));
+            end
+        end
+        printf("\n");
+    end
+endfunction
+
+figure(1); clf
+hex=     get_convergence_times('results_scalability_nodeconnect/hex_128.txt',7,1);
+hex=[hex;get_convergence_times('results_scalability_nodeconnect/hex_64.txt',7,2)];
+hex=[hex;get_convergence_times('results_scalability_nodeconnect/hex_32.txt',7,3)];
+hex=[hex;get_convergence_times('results_scalability_nodeconnect/hex_16.txt',7,4)];
+hex=[hex;get_convergence_times('results_scalability_nodeconnect/hex_8.txt',7,5)];
+title("Hexagon topology (best case ordering)");
+xlabel("Number of nodes");
+ylabel("Node connect time [s]");
+legend(["N_{max}=128","N_{max}=64","N_{max}=32","N_{max}=16","N_{max}=8"],4);
+printf("Hexagon topology (best case ordering)\n");
+print_convergence(hex);
+
+printf("\n\n\n");
+
+
+figure(2); clf
+rhex=      get_convergence_times('results_scalability_nodeconnect/rhex_128.txt',7,1);
+rhex=[rhex;get_convergence_times('results_scalability_nodeconnect/rhex_64.txt',7,2)];
+rhex=[rhex;get_convergence_times('results_scalability_nodeconnect/rhex_32.txt',7,3)];
+rhex=[rhex;get_convergence_times('results_scalability_nodeconnect/rhex_16.txt',7,4)];
+rhex=[rhex;get_convergence_times('results_scalability_nodeconnect/rhex_8.txt',7,5)];
+title("Reverse hexagon topology (worst case ordering)");
+xlabel("Number of nodes");
+ylabel("Node connect time [s]");
+legend(["N_{max}=128","N_{max}=64","N_{max}=32","N_{max}=16","N_{max}=8"],4);
+printf("Reverse hexagon topology (worst case ordering)\n");
+print_convergence(rhex);
---
+++ simulator/simulation_scripts/scalability_nodeconnect.sh
@@ -0,0 +1,179 @@
+#!/bin/bash
+
+function fail {
+    echo -e "\n\nFailed with error $1\n"
+    exit 1
+}
+
+function noenv {
+    echo -e "\nError: you do not have omnet++ environment variables set"
+    echo -e "Try something like this"
+    echo -e "\tcd <path where you have installed omnet++"
+    echo -e "\t. ./setenv\n\tcd -"
+    echo -e "Or put the setenv script of omnet++ in your bashrc"
+    exit 1
+}
+
+which omnetpp > /dev/null || noenv;
+
+# Compile the hexagonal topology generator
+cd ../tools || fail
+make || fail
+cd -
+
+# The perl scripts to measure convergence time rely on certain print_dbg,
+# so to make the experiment reproducible, use the known good print_dbg
+# settings
+cmp debug_settings.h ../../network_module/debug_settings.h || \
+        cp debug_settings.h ../../network_module
+
+# Compile the omnet++ simulator
+cd ../WandstemMac || fail
+MODE=debug make || fail
+cd -
+
+# Parameters
+# $1 number of nodes
+# $2 max number of nodes
+# $3 max number of hops
+# $4 simulation time
+# $5 connect|disconnect
+# $6 results file
+function hex_topology_experiment {
+    echo "topology=hex node=$1 maxnodes=$2 maxhops=$3 simtime=$4 $5"
+    
+    # Hardcoding connect/disconnect time to half simulation time
+    time=$(($4 / 2))
+    node=$(($1 - 1))
+    connstr="$node $5 $time"
+    
+    # NOTE: 0=hex
+    ../../tools/nedgen_hexagon "Hex$1_$2" $1 $2 $3 $4 0 "$connstr" > /dev/null || fail
+    
+    ../../WandstemMac/out/clang-debug/src/WandstemMac_dbg -m -u Cmdenv  \
+            -n .:../../WandstemMac/src "Hex$1_$2.ini"                   \
+            --cmdenv-express-mode=false --cmdenv-log-prefix="%l %o %N:" \
+            > /dev/null || fail
+    
+    perl ../../tools/postprocess.pl "results/Hex$1_$2.elog" || fail
+
+    convergencetime=`perl ../../tools/connecttime.pl "Hex$1_$2.ned" "results/Hex$1_$2.elog"`
+    
+    echo "$1 $2 $3 $convergencetime" >> "$6"
+}
+
+# Parameters
+# $1 number of nodes
+# $2 max number of nodes
+# $3 max number of hops
+# $4 simulation time
+# $5 connect|disconnect
+# $6 results file
+function rhex_topology_experiment {
+    echo "topology=rhex node=$1 maxnodes=$2 maxhops=$3 simtime=$4 $5"
+    
+    # Hardcoding connect/disconnect time to half simulation time
+    time=$(($4 / 2))
+    node=1
+    connstr="$node $5 $time"
+    
+    # NOTE: 1=rhex
+    ../../tools/nedgen_hexagon "RHex$1_$2" $1 $2 $3 $4 1 "$connstr" > /dev/null || fail
+    
+    ../../WandstemMac/out/clang-debug/src/WandstemMac_dbg -m -u Cmdenv  \
+            -n .:../../WandstemMac/src "RHex$1_$2.ini"                  \
+            --cmdenv-express-mode=false --cmdenv-log-prefix="%l %o %N:" \
+            > /dev/null || fail
+    
+    perl ../../tools/postprocess.pl "results/RHex$1_$2.elog" || fail
+
+    convergencetime=`perl ../../tools/connecttime.pl "RHex$1_$2.ned" "results/RHex$1_$2.elog"`
+    
+    echo "$1 $2 $3 $convergencetime" >> "$6"
+}
+
+# Create the results and temporary directory
+
+mkdir results_scalability_nodeconnect
+
+rm -rf temp
+mkdir temp || fail
+cd temp
+
+## Run the hex experiments for node connect
+
+rm -f "../results_scalability_nodeconnect/hex_128.txt"
+hex_topology_experiment   2 128 1 400 connect "../results_scalability_nodeconnect/hex_128.txt"
+hex_topology_experiment   4 128 1 400 connect "../results_scalability_nodeconnect/hex_128.txt"
+hex_topology_experiment   8 128 2 400 connect "../results_scalability_nodeconnect/hex_128.txt"
+hex_topology_experiment  16 128 2 400 connect "../results_scalability_nodeconnect/hex_128.txt"
+hex_topology_experiment  32 128 3 400 connect "../results_scalability_nodeconnect/hex_128.txt"
+hex_topology_experiment  64 128 5 800 connect "../results_scalability_nodeconnect/hex_128.txt"
+hex_topology_experiment 128 128 7 800 connect "../results_scalability_nodeconnect/hex_128.txt"
+
+rm -f "../results_scalability_nodeconnect/hex_64.txt"
+hex_topology_experiment   2  64 1 200 connect "../results_scalability_nodeconnect/hex_64.txt"
+hex_topology_experiment   4  64 1 200 connect "../results_scalability_nodeconnect/hex_64.txt"
+hex_topology_experiment   8  64 2 400 connect "../results_scalability_nodeconnect/hex_64.txt"
+hex_topology_experiment  16  64 2 400 connect "../results_scalability_nodeconnect/hex_64.txt"
+hex_topology_experiment  32  64 3 400 connect "../results_scalability_nodeconnect/hex_64.txt"
+hex_topology_experiment  64  64 5 400 connect "../results_scalability_nodeconnect/hex_64.txt"
+
+rm -f "../results_scalability_nodeconnect/hex_32.txt"
+hex_topology_experiment   2  32 1 200 connect "../results_scalability_nodeconnect/hex_32.txt"
+hex_topology_experiment   4  32 1 200 connect "../results_scalability_nodeconnect/hex_32.txt"
+hex_topology_experiment   8  32 2 400 connect "../results_scalability_nodeconnect/hex_32.txt"
+hex_topology_experiment  16  32 2 400 connect "../results_scalability_nodeconnect/hex_32.txt"
+hex_topology_experiment  32  32 3 400 connect "../results_scalability_nodeconnect/hex_32.txt"
+
+rm -f "../results_scalability_nodeconnect/hex_16.txt"
+hex_topology_experiment   2  16 1 200 connect "../results_scalability_nodeconnect/hex_16.txt"
+hex_topology_experiment   4  16 1 200 connect "../results_scalability_nodeconnect/hex_16.txt"
+hex_topology_experiment   8  16 2 200 connect "../results_scalability_nodeconnect/hex_16.txt"
+hex_topology_experiment  16  16 2 200 connect "../results_scalability_nodeconnect/hex_16.txt"
+
+rm -f "../results_scalability_nodeconnect/hex_8.txt"
+hex_topology_experiment   2   8 1 200 connect "../results_scalability_nodeconnect/hex_8.txt"
+hex_topology_experiment   4   8 1 200 connect "../results_scalability_nodeconnect/hex_8.txt"
+hex_topology_experiment   8   8 2 200 connect "../results_scalability_nodeconnect/hex_8.txt"
+
+## Run the rhex experiments
+
+rm -f "../results_scalability_nodeconnect/rhex_128.txt"
+rhex_topology_experiment   2 128 1 400 connect "../results_scalability_nodeconnect/rhex_128.txt"
+rhex_topology_experiment   4 128 1 400 connect "../results_scalability_nodeconnect/rhex_128.txt"
+rhex_topology_experiment   8 128 2 400 connect "../results_scalability_nodeconnect/rhex_128.txt"
+rhex_topology_experiment  16 128 2 400 connect "../results_scalability_nodeconnect/rhex_128.txt"
+rhex_topology_experiment  32 128 3 400 connect "../results_scalability_nodeconnect/rhex_128.txt"
+rhex_topology_experiment  64 128 5 800 connect "../results_scalability_nodeconnect/rhex_128.txt"
+rhex_topology_experiment 128 128 7 800 connect "../results_scalability_nodeconnect/rhex_128.txt"
+
+rm -f "../results_scalability_nodeconnect/rhex_64.txt"
+rhex_topology_experiment   2  64 1 200 connect "../results_scalability_nodeconnect/rhex_64.txt"
+rhex_topology_experiment   4  64 1 200 connect "../results_scalability_nodeconnect/rhex_64.txt"
+rhex_topology_experiment   8  64 2 400 connect "../results_scalability_nodeconnect/rhex_64.txt"
+rhex_topology_experiment  16  64 2 400 connect "../results_scalability_nodeconnect/rhex_64.txt"
+rhex_topology_experiment  32  64 3 400 connect "../results_scalability_nodeconnect/rhex_64.txt"
+rhex_topology_experiment  64  64 5 400 connect "../results_scalability_nodeconnect/rhex_64.txt"
+
+rm -f "../results_scalability_nodeconnect/rhex_32.txt"
+rhex_topology_experiment   2  32 1 200 connect "../results_scalability_nodeconnect/rhex_32.txt"
+rhex_topology_experiment   4  32 1 200 connect "../results_scalability_nodeconnect/rhex_32.txt"
+rhex_topology_experiment   8  32 2 400 connect "../results_scalability_nodeconnect/rhex_32.txt"
+rhex_topology_experiment  16  32 2 400 connect "../results_scalability_nodeconnect/rhex_32.txt"
+rhex_topology_experiment  32  32 3 400 connect "../results_scalability_nodeconnect/rhex_32.txt"
+
+rm -f "../results_scalability_nodeconnect/rhex_16.txt"
+rhex_topology_experiment   2  16 1 200 connect "../results_scalability_nodeconnect/rhex_16.txt"
+rhex_topology_experiment   4  16 1 200 connect "../results_scalability_nodeconnect/rhex_16.txt"
+rhex_topology_experiment   8  16 2 200 connect "../results_scalability_nodeconnect/rhex_16.txt"
+rhex_topology_experiment  16  16 2 200 connect "../results_scalability_nodeconnect/rhex_16.txt"
+
+rm -f "../results_scalability_nodeconnect/rhex_8.txt"
+rhex_topology_experiment   2   8 1 200 connect "../results_scalability_nodeconnect/rhex_8.txt"
+rhex_topology_experiment   4   8 1 200 connect "../results_scalability_nodeconnect/rhex_8.txt"
+rhex_topology_experiment   8   8 2 200 connect "../results_scalability_nodeconnect/rhex_8.txt"
+
+cd ..
+
+scilab -f plot_scalability_nodeconnect.sce || echo "Scilab not found, not plotting results"
