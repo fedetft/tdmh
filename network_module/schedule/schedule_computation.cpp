ecff1fc2a55716d4f15f79d1e5f339e83b15fd22
Terraneo Federico <federico.terraneo@polimi.it>
2018-09-18 14:26:42 +0200

    Fix compiling for Miosix

--- network_module/schedule/schedule_computation.cpp
+++ network_module/schedule/schedule_computation.cpp
@@ -25,6 +25,7 @@
  *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
  ***************************************************************************/
 
+#include <queue>
 #include "../debug_settings.h"
 #include "schedule_computation.h"
 #include "../uplink/stream_management/stream_management_context.h"
