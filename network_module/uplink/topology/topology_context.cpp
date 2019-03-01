fedetft/tdmh

commit ce4e3da994749e5692fb0ec2beda57283d520ab6

TopologyContext: Added RSSI check to Master

network_module/uplink/topology/topology_context.cpp
@@ -112,14 +112,24 @@ MasterTopologyContext::MasterTopologyContext(MACContext& ctx) : TopologyContext(
 void MasterTopologyContext::receivedMessage(UplinkMessage msg, unsigned char sender, short rssi) {
     // Lock mutex because TopologyMap is shared with ScheduleComputation thread
 #ifdef _MIOSIX
-        miosix::Lock<miosix::Mutex> lck(sched_mutex);
+    miosix::Lock<miosix::Mutex> lck(sched_mutex);
 #else
-        std::unique_lock<std::mutex> lck(sched_mutex);
+    std::unique_lock<std::mutex> lck(sched_mutex);
 #endif
-        if (neighborsUnseenFor.find(sender) == neighborsUnseenFor.end()) {
-            topology.addEdge(sender, 0);
-        }
-        neighborsUnseenFor[sender] = 0;
+    /*
+      NOTE: there is a corner case in which a node can decide to belong to hop 1
+      because from his side the RSSI is high enough, but the master has not (yet)
+      received a message with high enough RSSI. In this case, due to topology
+      forwarding the edge will eventually end up in the topology, without being
+      added through receivedMessage. For this reason, we MUST check with
+      topology.hasEdge here, so that in this case we will add the node also in
+      neighborsUnseenFor, this is done so neighborsUnseenFor and topology will be coherent.
+     */
+    if(!topology.hasEdge(sender,0)) {
+        if(rssi < ctx.getNetworkConfig().getMinNeighborRSSI()) return;
+        topology.addEdge(sender,0);
+    }
+    neighborsUnseenFor[sender] = 0;
 }
 
 void MasterTopologyContext::unreceivedMessage(unsigned char sender) {

network_module/uplink/topology/mesh_topology_context.cpp
@@ -44,6 +44,8 @@ void DynamicMeshTopologyContext::receivedMessage(UplinkMessage msg, unsigned cha
         neighbors.addNeighbor(sender);
         neighborsUnseenFor[sender] = 0;
     }
+    /* 28/20/2019 Fede&Izzo: the forwarded Topologies and SME
+       do not need the check on RSSI because this information is received from other nodes */
     auto it = neighborsUnseenFor.find(sender);
     if (it != neighborsUnseenFor.end())
         it->second = 0;
@@ -101,6 +103,8 @@ void DynamicMeshTopologyContext::checkEnqueueOrUpdate(ForwardedNeighborMessage* 
 
 void MasterMeshTopologyContext::receivedMessage(UplinkMessage msg, unsigned char sender, short rssi) {
     MasterTopologyContext::receivedMessage(msg, sender, rssi);
+    /* 28/20/2019 Fede&Izzo: the manageTopologyUpdate does not need the check on RSSI because
+       this topology information is received from other nodes */
     auto tMsg = static_cast<NeighborMessage*>(msg.getTopologyMessage());
     manageTopologyUpdate(sender, tMsg->getNeighbors());
     std::vector<ForwardedNeighborMessage*> fwds = tMsg->getForwardedTopologies();

commit a012846d6f549116fcf2715f94df9ca9194f312a

Fixed bug introduced with commit ce4e3da994749e5692fb0ec2beda57283d520ab6

network_module/uplink/topology/topology_context.cpp
@@ -125,7 +125,9 @@ void MasterTopologyContext::receivedMessage(UplinkMessage msg, unsigned char sen
       topology.hasEdge here, so that in this case we will add the node also in
       neighborsUnseenFor, this is done so neighborsUnseenFor and topology will be coherent.
      */
-    if(!topology.hasEdge(sender,0)) {
+    /* FIXME: This code should be if(!topology.hasEdge(sender,0)) because of the
+       note above but it breaks the topology. TODO: discover why */
+    if(neighborsUnseenFor.find(sender) == neighborsUnseenFor.end()) {
         if(rssi < ctx.getNetworkConfig().getMinNeighborRSSI()) return;
         topology.addEdge(sender,0);
     }