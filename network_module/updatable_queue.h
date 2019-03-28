6de9a11e0aacc7bf2971eb9785480859f824333b
Federico Terraneo <fede.tft@miosix.org>
2019-03-28 17:55:37 +0100

    Refactored UpdatableQueue

--- network_module/updatable_queue.h
+++ network_module/updatable_queue.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C)  2018 by Polidori Paolo                                 *
+ *   Copyright (C)  2018, 2019 by Polidori Paolo, Federico Terraneo        *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -27,150 +27,119 @@
 
 #pragma once
 
-#include <utility>
 #include <map>
-#include <deque>
-#include <algorithm>
+#include <list>
+#include <functional>
 #include <stdexcept>
 
 namespace mxnet {
 
 /**
- * A queue data structure in which the elements are enqueued with a relative key, and they can be updated
- * preserving the queue ordering of the old value. Elements must be unique by key.
- * @tparam keyType the type of the key to which the element is associated.
- * @tparam valType the type of the values stored in the queue.
+ * A queue data structure in which the elements are enqueued with a relative key,
+ * and they can be updated preserving the queue ordering of the old value.
+ * Elements are thus kept unique by key.
+ * \tparam K the type of the key to which the element is associated.
+ * \tparam V the type of the values stored in the queue.
  */
-template<class keyType, class valType>
-class UpdatableQueue {
+template<typename K, typename V>
+class UpdatableQueue
+{
 public:
-    UpdatableQueue() {
-        // TODO Auto-generated constructor stub
-
+    /**
+     * Adds an element to the queue, replacing a previous element with the
+     * same key, if present.
+     * \param key unique reference for the value
+     * \param val the value to be added/replaced
+     */
+    void enqueue(K key, const V& val);
+    
+    /**
+     * Adds an element to the queue, replacing a previous element with the
+     * same key, if present.
+     * \param key unique reference for the value
+     * \param val the value to be added
+     * \param f function that is called if the element to be added replaces a
+     * previous element
+     */
+    void enqueue(K key, const V& val, std::function<void (V& oldVal, const V& newVal)> f);
+    
+    /**
+     * \return the oldest element in the queue, without removing it
+     */
+    V& top() const;
+    
+    /**
+     * \return the oldest element in the queue, removing it
+     */
+    V dequeue();
+    
+    /**
+     * Remove all elements in the queue
+     */
+    void clear()
+    {
+        data.clear();
+        queue.clear();
     }
-    virtual ~UpdatableQueue() {
-        // TODO Auto-generated destructor stub
-    }
+    
     /**
-     * Extracts the next element in the queue.
-     * @return the next element in the queue.
+     * \return true if the queue is empty
      */
-    valType&& dequeue();
+    bool empty() const { return data.empty(); }
+    
     /**
-     * Returns the next element in the queue without removing it.
-     * @return the next element in the queue.
+     * \return the number of elements
      */
-    valType& top() const;
-    /**
-     * Adds an element to the queue.
-     * @param key unique reference for the value.
-     * @param val the value to be added.
-     */
-    bool enqueue(keyType key, const valType& val);
-    /**
-     * Removed an element based on its key, if present.
-     * @param key the key for which the value needs to be removed.
-     * @return if the element was present and removed
-     */
-    bool removeElement(keyType key);
-    /**
-     * Updates the value associated with a certain key.
-     * If no value is present, no modification will occur.
-     * @param key the identifier for the value.
-     * @param val the new value to be associated with the key.
-     * @return if any value was updated
-     */
-    bool update(keyType key, const valType& val);
-    /**
-     * Returns an element by its key. An exception is thrown is no value is associated with such key
-     * @param key the key identifying the value.
-     * @return the value associated with the key.
-     */
-    valType& getByKey(keyType key);
-    /**
-     * Returns an element by its key. An exception is thrown is no value is associated with such key
-     * @param key the key identifying the value.
-     * @return the value associated with the key.
-     */
-    const valType& getByKey(keyType key) const;
-    /**
-     * Checks whether an element related to the provided key is present.
-     * @param key the key for which the value presence is checked.
-     * @return if the value is present.
-     */
-    bool hasKey(keyType key) const;
-    /**
-     * Checks if any element is present in the queue.
-     * @return if any element is present.
-     */
-    bool isEmpty();
-
-    /** Returns the size of the %UpdatableQueue.  */
     std::size_t size() const { return data.size(); }
+    
 private:
-    std::map<keyType, valType> data;
-    std::deque<keyType> queue;
+    std::map<K,V> data;
+    std::list<K> queue;
 };
 
-template<class keyType, class valType>
-valType&& UpdatableQueue<keyType, valType>::dequeue() {
-    if (queue.empty()) throw std::runtime_error("no element in queue");
-    auto key = queue.back();
-    queue.pop_back();
-    auto retval = data[key];
-    data.erase(key);
-    return std::move(retval);
+template<typename K, typename V>
+void UpdatableQueue<K,V>::enqueue(K key, const V& val)
+{
+    auto it = data.find(key);
+    if(it != data.end())
+    {
+        it->second = val; //Replace
+    } else {
+        data[key] = val;
+        queue.push_front(key);
+    }
 }
 
-template<class keyType, class valType>
-valType& UpdatableQueue<keyType, valType>::top() const {
-    if (queue.empty()) throw std::runtime_error("no element in queue");
+template<typename K, typename V>
+void UpdatableQueue<K,V>::enqueue(K key, const V& val, std::function<void (V& oldVal, const V& newVal)> f)
+{
+    auto it = data.find(key);
+    if(it != data.end())
+    {
+        f(it->second, val);
+        it->second = val; //Replace
+    } else {
+        data[key] = val;
+        queue.push_front(key);
+    }
+}
+
+template<typename K, typename V>
+V& UpdatableQueue<K,V>::top() const
+{
+    if(data.empty()) throw std::runtime_error("no element in queue");
     return data[queue.back()];
 }
 
-template<class keyType, class valType>
-bool UpdatableQueue<keyType, valType>::enqueue(keyType key, const valType& val) {
-    if(hasKey(key)) return false;
-    data[key] = val;
-    queue.push_front(key);
-    return true;
-}
-
-template<class keyType, class valType>
-bool UpdatableQueue<keyType, valType>::removeElement(keyType key) {
-    if(!hasKey(key)) return false;
+template<typename K, typename V>
+V UpdatableQueue<K,V>::dequeue()
+{
+    if(data.empty()) throw std::runtime_error("no element in queue");
+    auto key = queue.back();
+    auto result = data[key];
+    queue.pop_back();
     data.erase(key);
-    queue.erase(std::find(queue.begin(), queue.end(), key));
-    return true;
+    return result;
 }
 
-template<class keyType, class valType>
-bool UpdatableQueue<keyType, valType>::update(keyType key, const valType& val) {
-    if(!hasKey(key)) return false;
-    data[key] = val;
-    return true;
-}
-
-template<class keyType, class valType>
-inline const valType& UpdatableQueue<keyType, valType>::getByKey(keyType key) const {
-    if (!hasKey(key)) throw std::runtime_error("empty");
-    return data.at(key);
-}
-
-template<class keyType, class valType>
-inline valType& UpdatableQueue<keyType, valType>::getByKey(keyType key) {
-    if (!hasKey(key)) throw std::runtime_error("empty");
-    return data.at(key);
-}
-
-template<class keyType, class valType>
-inline bool UpdatableQueue<keyType, valType>::hasKey(keyType key) const {
-    return data.count(key);
-}
-
-template<class keyType, class valType>
-bool UpdatableQueue<keyType, valType>::isEmpty() {
-    return data.size() == 0;
-}
-
-} /* namespace mxnet */
+} // namespace mxnet
