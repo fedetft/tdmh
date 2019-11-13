3e3a9a80cf086e99ed305f7a0c394fb58aa2e035
Federico Terraneo <fede.tft@miosix.org>
2019-11-13 13:49:08 +0100

    Improved directneighbor-rssi.pl

--- experiments/scripts/directneighbor-rssi.pl
+++ experiments/scripts/directneighbor-rssi.pl
@@ -3,14 +3,28 @@
 use warnings;
 use strict;
 
-die "use: perl directneighbor-rssi.pl node < logfile" unless($#ARGV+1==1);
+my $numargs=$#ARGV+1;
+die "use: perl directneighbor-rssi.pl node <uplinkperiod> < logfile"
+    unless($numargs==1 || $numargs==2);
 my $node=$ARGV[0];
+my $period=-1;
+$period=$ARGV[1] if($numargs==2);
 
-my $line=0;
+my $previous=-1;
+my $slack=0.0001;
 
 while(<STDIN>){
-    $line++;
-    next unless(/\[U\]<-N=(\d+) @\d+ (-?\d+)dBm/);
+    next unless(/\[U\]<-N=(\d+) @(\d+) (-?\d+)dBm/);
     next unless($1==$node);
-    print "$line $2\n";
+    my $time=$2/1000000000.0;
+    my $rssi=$3;
+    if($period>0 && $previous>0) {
+        # If we are given the uplink period, print Nan if uplink packets are missed
+        while($previous+$period+$slack<$time) {
+            $previous+=$period;
+            print "$previous Nan\n";
+        }
+    }
+    print "$time $rssi\n";
+    $previous=$time;
 }
