c5e2190c18642c126e325fee29f21063e302e50f
Valeria <valeria.mazzola@mail.polimi.it>
2020-03-19 11:05:06 +0100

    Glued tiny-AES-C to AESAccelerator for single block encryption
           NOTE: code from [https://github.com/bricke/tiny-AES-C]
    
           NOTE: single block decryption is still not supported in
           simulator, but probably will never be needed

--- network_module/util/aes_accelerator.h
+++ network_module/util/aes_accelerator.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <miosix.h>
+#include "tiny_aes_c.h"
 
 namespace mxnet {
 
@@ -24,6 +25,8 @@ public:
 #ifdef _MIOSIX
         AES->CTRL &= ~AES_CTRL_DECRYPT;
         processBlock(ctx, ptx);
+#else
+        AES_ECB_encrypt((const uint8_t*)ptx, key, (uint8_t*)ctx, (const uint32_t)16);
 #endif
     }
     /* Decrypt a single block. Expect buffer to be 16 bytes long.
@@ -40,6 +43,10 @@ private:
     AESAccelerator();
 
     void processBlock(void *dst, const void *src);
+
+#ifndef _MIOSIX
+    unsigned char key[16];
+#endif
 
 };
 
