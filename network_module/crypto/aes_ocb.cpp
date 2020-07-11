f0569c2fb39e41ae7df363a80c9727b517c121f9
Valeria Mazzola <valeria.mazzola@mail.polimi.it>
2020-07-11 15:15:55 +0200

    AesOcb: moving function to cpp file

--- network_module/crypto/aes_ocb.cpp
+++ network_module/crypto/aes_ocb.cpp
@@ -13,6 +13,26 @@ bool AesOcb::verifyAndDecrypt(const void *tag, void *ptx, const void *ctx,
                               unsigned int authLength) {
 }
 
+void AesOcb::computeFirstOffset() {
+    memcpy(lastNonce, nonce, 16);
+    // select the last 6 bits
+    unsigned char bottom = nonce[15] & 0x3f;
+    // clear the last 6 bits
+    nonce[15] = nonce[15] & 0xc0;
+    unsigned char ktop[24];
+    aes.ecbEncrypt(ktop, nonce);
+    memcpy(ktop + 16, ktop, 8);
+    xorBytes(ktop + 16, ktop + 16, ktop + 1, 8);
+
+    unsigned char bitshift = bottom % 8;
+    unsigned char byteshift = bottom / 8;
+    unsigned char carry;
+    for (int i=15; i>=0; i--) {
+        carry = ktop[i+byteshift+1] >> (8-bitshift);
+        offset0[i] = (ktop[i+byteshift] << bitshift) || carry ;
+    }
+}
+
 void AesOcb::gfDouble(unsigned char dst[16], const unsigned char src[16]) {
     // Select MSB
     unsigned char bit = (src[0] & (1<<7)) >> 7;
