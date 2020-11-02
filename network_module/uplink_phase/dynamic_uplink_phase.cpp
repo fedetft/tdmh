dd6f6ac73cca79472b95bbac9379af6c9424b219
Valeria Mazzola <valeria.mazzola@mail.polimi.it>
2020-11-02 17:06:16 +0100

    GPL notices and copyright updates

--- network_module/uplink_phase/dynamic_uplink_phase.cpp
+++ network_module/uplink_phase/dynamic_uplink_phase.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
- *   Copyright (C) 2018-2019 by Polidori Paolo, Terraneo Federico,         *
- *   Federico Amedeo Izzo                                                  *
+ *   Copyright (C) 2018-2020 by Polidori Paolo, Terraneo Federico,         *
+ *   Federico Amedeo Izzo, Valeria Mazzola                                 *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
--- network_module/util/updatable_queue.h
+++ network_module/util/updatable_queue.h
@@ -1,5 +1,6 @@
 /***************************************************************************
- *   Copyright (C)  2018, 2019 by Polidori Paolo, Federico Terraneo        *
+ *   Copyright (C)  2018, 2019 by Polidori Paolo, Federico Terraneo,       *
+ *   Valeria Mazzola                                                       *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
