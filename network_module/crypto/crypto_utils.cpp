71202160512f46f245536b90b8d358de974e8964
Federico Terraneo <fede.tft@miosix.org>
2021-05-20 19:11:00 +0200

    Make OCB3 40% faster

--- network_module/crypto/crypto_utils.cpp
+++ network_module/crypto/crypto_utils.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C)  2020 by Valeria Mazzola                                *
+ *   Copyright (C)  2020 by Valeria Mazzola, Federico Terraneo             *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -25,12 +25,44 @@
  *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
  ***************************************************************************/
 
-#pragma once
+#include <cassert>
+#include "crypto_utils.h"
 
 namespace mxnet {
 
-void xorBytes(void *dst, const void *op1, const void *op2, unsigned int length);
+void xorBytes(void *dst, const void *op1, const void *op2, unsigned int length) {
+    auto d = reinterpret_cast<unsigned int*>(dst);
+    auto o1 = reinterpret_cast<const unsigned int*>(op1);
+    auto o2 = reinterpret_cast<const unsigned int*>(op2);
 
-void secureClearBytes(void *bytes, unsigned int length);
+    auto l = length / 4;
+    for (unsigned i=0; i<l; i++) {
+        *d++ = *o1++ ^ *o2++;
+    }
+
+    auto db = reinterpret_cast<unsigned char*>(d);
+    auto o1b = reinterpret_cast<const unsigned char*>(o1);
+    auto o2b = reinterpret_cast<const unsigned char*>(o2);
+    
+    l = length & 0b11;
+    for (unsigned i=0; i<l; i++) {
+        *db++ = *o1b++ ^ *o2b++;
+    }
+}
+
+void secureClearBytes(void *bytes, unsigned int length) {
+    auto p = reinterpret_cast<volatile unsigned int*>(bytes);
+
+    auto l = length / 4;
+    for (unsigned i=0; i<l; i++) {
+        *p++ = 0;
+    }
+    
+    auto pb = reinterpret_cast<volatile unsigned char*>(p);
+    l = length & 0b11;
+    for (unsigned i=0; i<l; i++) {
+        *pb++ = 0;
+    }
+}
 
 } //namespace mxnet
