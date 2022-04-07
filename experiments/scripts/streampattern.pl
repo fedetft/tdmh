a75d6c752037a78fbe018f9783197b68de56f93c
luca-conterio <luca.cont96@gmail.com>
2022-04-07 17:32:52 +0200

    [scripts] Added received and missed count in streampattern

--- experiments/scripts/streampattern.pl
+++ experiments/scripts/streampattern.pl
@@ -4,23 +4,32 @@ use warnings;
 use strict;
 
 die "use: perl streampattern.pl streamsrc streamdst < logfile" unless($#ARGV+1==2);
-my $streamsrc=$ARGV[0];
-my $streamdst=$ARGV[1];
+my $streamsrc = $ARGV[0];
+my $streamdst = $ARGV[1];
 
-print "stream [$streamsrc-$streamdst]\n";
+my $receivedCount = 0;
+my $missedCount   = 0;
 
 while(<STDIN>)
 {
     if(/\[A\] Received data from \((\d+),(\d+)\)/ ||
        /\[A\] R \((\d+),(\d+)\)/) {
         my $src=$1, my $dst=$2;
-        print "1" if $src==$streamsrc && $dst==$streamdst;
+        if ($src==$streamsrc && $dst==$streamdst) {
+            print "1";
+            $receivedCount++;
+        }
 
     } elsif(/\[E\] No data received from Stream \((\d+),(\d+)\)/ ||
             /\[E\] M \((\d+),(\d+)\)/) {
         my $src=$1, my $dst=$2;
-        print "0" if $src==$streamsrc && $dst==$streamdst;
+        if ($src==$streamsrc && $dst==$streamdst) {
+            print "0";
+            $missedCount++;
+        }
     }
 }
 
+print "\n\n";
+print "Stream [$streamsrc-$streamdst]: RECEIVED=${receivedCount} MISSED=${missedCount}\n";
 print "\n";
